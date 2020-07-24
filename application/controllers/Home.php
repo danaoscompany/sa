1a4f9cb66ad49e4dbd6736f59788d02521ed1a50
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-07-25 03:44:27 +0700

    Last commit

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends CI_Controller {
 		if ($this->session->logged_in == 1) {
 			$this->load->view('home');
 		} else {
-			header("Location: http://skinmed.id/skinmed/sa/login");
+			header("Location: http://skinmed.id/sa/login");
 		}
 	}
 }
