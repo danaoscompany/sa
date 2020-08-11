ec735ae2493cb3b008c1cf20701d8106a4f0ff8c
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2020-08-12 03:52:45 +0700

    Last commit

--- application/controllers/Common.php
+++ application/controllers/Common.php
@@ -3,13 +3,14 @@
 class Common extends CI_Controller {
 
 	public function index() {
-		if ($this->session->logged_in == 1) {
+		echo "Halo dunia 123";
+		/*if ($this->session->logged_in == 1) {
 			$userID = $this->session->user_id;
 			$this->load->view('common', array(
 				'userID' => $userID
 			));
 		} else {
 			header("Location: http://skinmed.id/sa/devices");
-		}
+		}*/
 	}
 }
